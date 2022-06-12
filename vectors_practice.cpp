83b33360ff42a919cf9504b1eabc827b0b180cca
Teja Marneni <63079069+tejamarneni@users.noreply.github.com>
2022-06-12 10:29:09 -0400

    Add files via upload

---
+++ vectors_practice.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+int main(){
+
+    vector<float> items;
+    int n;
+    float j;
+
+    cout << "Enter the size of Vector:\n";
+    cin >> n;
+    cout << endl;
+
+    for(int i = 0; i < n; i++){
+        cout << "Enter the number:\n";
+        cin >> j;
+        items.push_back(j);
+    }
+
+    cout << endl;
+
+     for(int i = 0; i < n; i++)
+        cout << items[i] << endl;;
+    
+}
